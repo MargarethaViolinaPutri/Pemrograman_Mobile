2aaef48cc90bce0b59b2c5b8c04c099331f92590
MargarethaViolinaPutri <haeretha15@gmail.com>
2023-08-30 11:39:44 +0700

    first commit

--- PERTEMUAN 1/tugas1/lib/main.dart
+++ PERTEMUAN 1/tugas1/lib/main.dart
@@ -28,10 +28,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 13, 137, 237)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Tugas Pertama Flutter'),
     );
   }
 }
@@ -106,12 +107,15 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Margaretha Violina Putri Purnomo',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const Text(
+              '2141762056',
             ),
+            //Text(
+            //  '$_counter',
+            //  style: Theme.of(context).textTheme.headlineMedium,
+            //),
           ],
         ),
       ),
